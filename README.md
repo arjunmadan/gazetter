da77313f581e3238e521abd88d5dad7aaded4607
Arjun Madan <arjunmadan1992@gmail.com>
2023-03-06 22:14:14 -0500

    Make output dir customizable

--- README.md
+++ README.md
@@ -3,3 +3,19 @@ Get articles from the Indian Gazette
 
 ## Usage
 
+Download the correct binary from the [releases tab](https://github.com/arjunmadan/gazetter/releases/latest).
+
+The CLI takes in 4 arguments:
+
+- url: Base URL to make requests to. Defaults to https://egazette.nic.in/WriteReadData
+- start: Starting range of article ID. Defaults to 160000.
+- end: Ending range of article ID. Defaults to 180000.
+- year: Year when the articles were published. Defaults to 2016.
+- dir: Directory where the PDFs will be saved.
+
+
+Example Usage:
+
+```bash
+./gazetter -url <url> -year 2020 -start 200000 -end 220000 -dir /Users/arjunmadan/Downloads
+```
--- gazetter.go
+++ gazetter.go
@@ -4,23 +4,30 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
+	"path"
 )
 
 func main() {
 
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal("Unable to get home directory")
+	}
 	url := flag.String("url", "https://egazette.nic.in/WriteReadData", "Base URL to make requests to (defaults to https://egazette.nic.in/WriteReadData/)")
 	year := flag.Int("year", 2016, "Year the articles were published (defaults to 2016)")
 	start := flag.Int("start", 160000, "Starting range of article ID (defaults to 160000)")
 	end := flag.Int("end", 180000, "Ending range of article ID (defaults to 180000)")
-
+	dir := flag.String("dir", homeDir, "Directory where the PDFs will be saved (defaults to your home directory)")
 	flag.Parse()
 
 	for i := *start; i <= *end; i++ {
 		requestUrl := fmt.Sprintf("%s/%d/%d.pdf", *url, *year, i)
 		filename := fmt.Sprintf("%d.pdf", i)
-		err := downloadFile(filename, requestUrl)
+		filepath := path.Join(*dir, filename)
+		err := downloadFile(filepath, requestUrl)
 		if err != nil {
 			fmt.Printf("error: %s\n", requestUrl)
 		}
